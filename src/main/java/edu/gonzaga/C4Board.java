a84be6022fcf785da5d8bb055ea1f12a7fc480d9
samps7 <samboy@gmail.com>
2023-04-09 20:42:51 -0700

    working coin placement (text-based)

--- src/main/java/edu/gonzaga/C4Board.java
+++ src/main/java/edu/gonzaga/C4Board.java
@@ -2,11 +2,60 @@ package edu.gonzaga;
 
 public class C4Board 
 {
-    private Coin[][] grid = new Coin[7][6];
+    private Coin[][] grid = new Coin[6][7];
     //private int[][] grid = new int[7][6]; // we will replace with "Coins" for actual
     public C4Board()
     {
         
+    }
+
+    public boolean acceptCoin(Coin c, int col)
+    {
+        int bot = 6;
+        if(col > 6 || col < 0)
+        {
+            return false;
+        }
+        for(int i = 5; i >= 0; i--)
+        {
+            if(grid[i][col] == null)
+            {
+                bot = i;
+            }
+            else
+            {
+                break;
+            }
+        }
+        if(bot < 6 && bot > -1)
+        {
+            grid[bot][col] = c;
+            return true;
+        }
+        return false;
+    }
+
+    public String boardDisplay()
+    {
+        String str = "This is the board: \n";
+        for(int i = grid.length - 1; i >= 0; i--)
+        {
+            for(int j = 0; j < grid[0].length; j++)
+            {
+                if(grid[i][j] == null)
+                {
+                    str += " - ";
+                }
+                else
+                {
+                    str += " " + grid[i][j].getLetter() + " ";
+                }
+            }
+            str += "\n";
+        }
+        str += " 0  1  2  3  4  5  6 ";
+
+        return str;
     }
 
     
--- src/main/java/edu/gonzaga/C4Game.java
+++ src/main/java/edu/gonzaga/C4Game.java
@@ -1,6 +1,7 @@
 package edu.gonzaga;
 
 import java.util.Scanner;
+import java.lang.Math;
 
 public class C4Game 
 {
@@ -10,6 +11,7 @@ public class C4Game
     private int moveCount = 0; // for while loop
     private boolean p1Win = false;
     private boolean p2Win = false;
+    private int playerTurn = 1;
 
 
     public C4Game(int mode1)
@@ -24,13 +26,12 @@ public class C4Game
             System.out.println("Enter P1 name");
             // get name from UI textbox here
             // get coin from combobox here
-            p1 = new Player(scan1.nextLine(), new Coin("1"));
-            
+            p1 = new Player(scan1.nextLine(), new Coin("X"));
 
             System.out.println("Enter P2 name");
             // get name from UI textbox here
             // get coin from combobox here
-            p2 = new Player(scan1.nextLine(), new Coin("2"));
+            p2 = new Player(scan1.nextLine(), new Coin("O"));
             
         }
         else
@@ -38,7 +39,7 @@ public class C4Game
             System.out.println("Enter P1 name");
             // get from UI textbox here
             // get coin from combobox here
-            p1 = new Player(scan1.nextLine(), new Coin("1"));
+            p1 = new Player(scan1.nextLine(), new Coin("X"));
 
             if(mode == 0)
             {
@@ -57,11 +58,58 @@ public class C4Game
 
     public void startGame()
     {
-        //Coin flip for who starts
+        Scanner scan1 = new Scanner(System.in); // remove this once UI working
+        C4Board grid = new C4Board();
 
+        //modifiers applied here (will be used in 1P games)
+        if(mode == 0)
+        {
+            // intro ez bot if needed
+        }
+        else if(mode == 1)
+        {
+            // intro med bot if needed
+        }
+        else if(mode == 2)
+        {
+            // init hard bot if needed
+        }
+        
+        //Coin flip for who starts
+        playerTurn = (int) Math.random()*2;
+        
         while(!p1Win && !p2Win && moveCount < 42)
         {
-            
+            if(playerTurn == 0)
+            {
+                System.out.println(p1.getName() + "'s turn");
+            }
+            else
+            {
+                System.out.println(p2.getName() + "'s turn");
+            }
+
+            System.out.println();
+            System.out.println(grid.boardDisplay());
+            System.out.println();
+            System.out.println("Which column do you want to place a piece?");
+
+            if(playerTurn == 0)
+            {
+                while(!grid.acceptCoin(p1.getCoin(), scan1.nextInt()))
+                {
+                    System.out.println("invalid  column");
+                }
+            }
+            else
+            {
+                while(!grid.acceptCoin(p2.getCoin(), scan1.nextInt()))
+                {
+                    System.out.println("invalid  column");
+                }
+            }
+            playerTurn++;
+            playerTurn = playerTurn % 2;
 
             moveCount++;
         }
