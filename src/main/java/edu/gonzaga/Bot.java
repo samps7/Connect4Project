30333fb401d61977d1fb43a7e1b59fe46945f050
samps7 <samboy@gmail.com>
2023-04-17 12:34:24 -0700

    oops pushed wrong version last time

--- src/main/java/edu/gonzaga/Bot.java
+++ src/main/java/edu/gonzaga/Bot.java
@@ -2,6 +2,9 @@ package edu.gonzaga;
 
 import com.gargoylesoftware.htmlunit.*;
 import com.gargoylesoftware.htmlunit.html.*;
+
+import org.apache.xalan.templates.ElemSort;
+
 import java.util.*;
 
 public class Bot extends Player
@@ -14,94 +17,203 @@ public class Bot extends Player
     @Override
     public int getMove( String s)
     {
-        WebClient webClient = new WebClient(BrowserVersion.CHROME);
-        HtmlPage page;
-        String url = "https://connect4.gamesolver.org/";
-        boolean[] flags = new boolean[7];
-
-        if(s.length() > 0) // acted on this
+        if(s.length() > 1)
         {
-            url += "?pos=" + s;
-            for(int i = 1; i <= 7; i++)
+            WebClient webClient = new WebClient(BrowserVersion.CHROME);
+            HtmlPage page;
+            String url = "https://connect4.gamesolver.org/";
+            boolean[] flags = new boolean[7];
+
+            if(s.length() > 0) // acted on this
             {
-                if(intCount(s, i) >= 6)
+                url += "?pos=" + s;
+                for(int i = 1; i <= 7; i++)
                 {
-                    flags[i-1] = true;
+                    if(intCount(s, i) >= 6)
+                    {
+                        flags[i-1] = true;
+                    }
                 }
+            }
+
+            try {
+                webClient.getOptions().setCssEnabled(false);
+                webClient.getOptions().setJavaScriptEnabled(true);
+                page = webClient.getPage(url);
+                webClient.waitForBackgroundJavaScript(1000);
+                String pageText = page.asXml();
+                pageText = pageText.substring(pageText.indexOf("sol0"), pageText.indexOf("class=\"board\""));
+                String[] lines = pageText.split(System.getProperty("line.separator"));
+                ArrayList<Integer[]> scores = new ArrayList<Integer[]>();
+                ArrayList<Integer[]> best = new ArrayList<Integer[]>();
+                int max = -1000;
+                //int maxIndex = -4;
+                int pos = 0;
+                for(int i = 0; i < lines.length; i++)
+                {
+                    while(pos < 7 && flags[pos] == true)
+                    {
+                        pos++;
+                    }
+                    if(isNumeric(lines[i].replaceAll("\\s", "")))
+                    {
+                        Integer[] mapping = new Integer[2];
+                        mapping[0] = pos;
+                        
+                        mapping[1] = Integer.parseInt(lines[i].replaceAll("\\s", ""));
+                        //System.out.println(Integer.parseInt(lines[i].replaceAll("\\s", "")));
+                        scores.add( mapping );
+                        if(Integer.parseInt(lines[i].replaceAll("\\s", "")) > max)
+                        {
+                            max = Integer.parseInt(lines[i].replaceAll("\\s", ""));
+                        }
+                        pos++;
+                    }
+                }
+                
+                // the following two are to randomize move selection 
+                // to make it harder for players to learn patterns
+
+                //if best <= 0 then do a move equivalent to best move
+
+                // if best > 0 then it can go to any move > 0
+
+                if(max <= 0)
+                {
+                    for(int i = 0; i < scores.size(); i++)
+                    {
+                        if(scores.get(i)[1] == max)
+                        {
+                            best.add(scores.get(i));
+                        }
+                    }
+                    return best.get((int)( Math.random()*best.size()))[0];
+                }
+                
+                else if(max > 0)
+                {
+                    for(int i = 0; i < scores.size(); i++)
+                    {
+                        if(scores.get(i)[1] > 0)
+                        {
+                            best.add(scores.get(i));
+                        }
+                    }
+                    return best.get((int)( Math.random()*best.size()))[0];
+                }
+
+                return -100;
+            }
+            catch (Exception ex) {
+                ex.printStackTrace();
+                // might wanna give a wifi error and quit
+                return -10;
             }
         }
-
-        try {
-            webClient.getOptions().setCssEnabled(false);
-            webClient.getOptions().setJavaScriptEnabled(true);
-            page = webClient.getPage(url);
-            webClient.waitForBackgroundJavaScript(400); // put this in a while loop maybe
-            String pageText = page.asXml();
-            pageText = pageText.substring(pageText.indexOf("sol0"), pageText.indexOf("class=\"board\""));
-            String[] lines = pageText.split(System.getProperty("line.separator"));
-            ArrayList<Integer[]> scores = new ArrayList<Integer[]>();
-            ArrayList<Integer[]> best = new ArrayList<Integer[]>();
-            int max = -1000;
-            //int maxIndex = -4;
-            int pos = 0;
-            for(int i = 0; i < lines.length; i++)
-            {
-                while(pos < 7 && flags[pos] == true)
-                {
-                    pos++;
-                }
-                if(isNumeric(lines[i].replaceAll("\\s", "")))
-                {
-                    Integer[] mapping = new Integer[2];
-                    mapping[0] = pos;
-                    
-                    mapping[1] = Integer.parseInt(lines[i].replaceAll("\\s", ""));
-                    //System.out.println(Integer.parseInt(lines[i].replaceAll("\\s", "")));
-                    scores.add( mapping );
-                    if(Integer.parseInt(lines[i].replaceAll("\\s", "")) > max)
-                    {
-                        max = Integer.parseInt(lines[i].replaceAll("\\s", ""));
-                    }
-                    pos++;
-                }
-            }
+        /*
+            For first move we can hard code it to significantly reduce time.
             
-            // the following two are to randomize move selection 
-            // to make it harder for players to learn patterns
-
-            //if best <= 0 then do a move equivalent to best move
-
-            // if best > 0 then it can go to any move > 0
-
-            if(max <= 0)
+            This is because move time exponentially decreases with the first move
+            having the most board state combinations that it has to check
+         */
+        else 
+        {
+            if(s.equals("1"))
             {
-                for(int i = 0; i < scores.size(); i++)
+                int rand = (int) Math.random()*4;
+                if(rand == 0)
                 {
-                    if(scores.get(i)[1] == max)
-                    {
-                        best.add(scores.get(i));
-                    }
+                    return 1;
                 }
-                return best.get((int)( Math.random()*best.size()))[0];
+                else if(rand == 1)
+                {
+                    return 2;
+                }
+                else if(rand == 2)
+                {
+                    return 3;
+                }
+                else
+                {
+                    return 5;
+                }
             }
-            
-            else if(max > 0)
+            else if(s.equals("2"))
             {
-                for(int i = 0; i < scores.size(); i++)
-                {
-                    if(scores.get(i)[1] > 0)
-                    {
-                        best.add(scores.get(i));
-                    }
-                }
-                return best.get((int)( Math.random()*best.size()))[0];
+                return 2;
             }
-
-            return -100;
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
-            return -10;
+            else if(s.equals("3"))
+            {
+                int rand = (int) Math.random()*4;
+                if(rand == 0)
+                {
+                    return 2;
+                }
+                else if(rand == 1)
+                {
+                    return 3;
+                }
+                else if(rand == 2)
+                {
+                    return 4;
+                }
+                else
+                {
+                    return 5;
+                }
+            }
+            else if(s.equals("4"))
+            {
+                return 3;
+            }
+            else if(s.equals("5"))
+            {
+                int rand = (int) Math.random()*4;
+                if(rand == 0)
+                {
+                    return 1;
+                }
+                else if(rand == 1)
+                {
+                    return 2;
+                }
+                else if(rand == 2)
+                {
+                    return 3;
+                }
+                else
+                {
+                    return 4;
+                }
+            }
+            else if(s.equals("6"))
+            {
+                return 2;
+            }
+            else if(s.equals("7"))
+            { 
+                int rand = (int) Math.random()*4;
+                if(rand == 0)
+                {
+                    return 1;
+                }
+                else if(rand == 1)
+                {
+                    return 3;
+                }
+                else if(rand == 2)
+                {
+                    return 4;
+                }
+                else
+                {
+                    return 5;
+                }  
+            }
+            else
+            {
+                return -1; // error
+            }
         }
     }
 
